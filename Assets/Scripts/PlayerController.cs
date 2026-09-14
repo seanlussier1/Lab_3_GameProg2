1fdbd83e53d8c851534ebbe753b88fb782ca80a0
seanlussier1 <158488641+seanlussier1@users.noreply.github.com>
2026-09-14 08:58:34 -0400

    Lab3 finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10f;
     private Rigidbody rb;
 
+    private Vector3 startPosition;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startPosition = transform.position;
     }
+
 
     void FixedUpdate() // Physics-related operations should go here
     {
@@ -21,5 +25,19 @@ public class PlayerController : MonoBehaviour
 
         // Apply movement to the rigidbody
         rb.AddForce(movement * moveSpeed, ForceMode.Force);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            Debug.Log("You Win");
+        }
+        else if (other.CompareTag("DeathPlane"))
+        {
+            rb.MovePosition(startPosition);
+            rb.linearVelocity = Vector3.zero;   // rb.velocity in older Unity versions
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 }
